e0818d4c518f51b126700d4573275f44c9ddedf9
Hayden <lilwilsond2@gmail.com>
2018-09-21 05:17:21 -0500

    a bit of formatting

--- bot.py
+++ bot.py
@@ -1,41 +1,49 @@
-from discord.ext import commands
-import settings.config
-import requests
 import json
+
+import requests
+from discord.ext import commands
+
+import settings.config
 
 description = '''a dumb bot'''
 bot = commands.Bot(command_prefix='!', description=description)
+
 
 def armor_lookup(skill: str):
     requestUrl = "https://mhw-db.com/armor?p={\"name\": true, \"skills.skillName\": true, \"skills.level\": true}"
     apiData = requests.get(requestUrl).text
     apiObjects = json.loads(apiData)
-    outputObjects = []
-    for thing in apiObjects:
-        for armorSkill in thing['skills']:
+    filteredArmor = []
+    for armorPiece in apiObjects:
+        for armorSkill in armorPiece['skills']:
             if armorSkill['skillName'] == skill:
-                outputObjects.append(thing)
-    output = [x['name'] for x in outputObjects]
+                filteredArmor.append(armorPiece)
+    output = [armorPiece['name'] for armorPiece in filteredArmor]
     return output
+
 
 @bot.event
 async def on_ready():
     print('hi')
+
 
 @bot.command()
 async def hunt(ctx, monsterName: str):
     """Looks up a monster and displays it's weaknesses."""
     await ctx.send('we\'re looking for the ' + monsterName + ' monster right now')
 
+
 @bot.command()
 async def armor(ctx, skill: str):
     """Finds all armor that has given skill"""
     await ctx.send(armor_lookup(skill))
 
+
 @bot.command()
 async def bees(ctx):
     """Talks about bees"""
     await ctx.send('Get on that shit yo!')
+
 
 @bot.command()
 async def menu(ctx):
@@ -43,4 +51,6 @@ async def menu(ctx):
     await ctx.send('!hunt')
     await ctx.send('!armor')
     await ctx.send('!bees')
+
+
 bot.run(settings.config.TOKEN)
