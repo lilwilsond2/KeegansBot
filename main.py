bd175ca31a6de800bb9ce7e77e0715e23e577e70
Hayden <lilwilsond2@gmail.com>
2018-09-21 02:44:28 -0500

    bot should at least be functional, yet useless

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from discord.ext import commands
 import settings.config
 
-bot = commands.Bot(command_prefix="!")
+description = '''a dumb bot'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('hi')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
 bot.run(settings.config.TOKEN)
